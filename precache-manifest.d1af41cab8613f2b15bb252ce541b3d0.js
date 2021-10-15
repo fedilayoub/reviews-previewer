5963993d39a9ec1250e5b79988bbc169f4f03352
fedilayoub <ayoubzahi2016@gmail.com>
2021-10-15 14:52:35 +0000

    Updates

---
+++ precache-manifest.d1af41cab8613f2b15bb252ce541b3d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "932a5e97addeb03644e472e01f19a8cb",
+    "url": "/reviews-previewer/index.html"
+  },
+  {
+    "revision": "ff8d2c6288d96bbf6ec5",
+    "url": "/reviews-previewer/static/css/main.b8bc303b.chunk.css"
+  },
+  {
+    "revision": "8bdd4952029fcd28ed3e",
+    "url": "/reviews-previewer/static/js/2.19c30a64.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/reviews-previewer/static/js/2.19c30a64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff8d2c6288d96bbf6ec5",
+    "url": "/reviews-previewer/static/js/main.6d6fd8bc.chunk.js"
+  },
+  {
+    "revision": "e0d36f25d8193d589965",
+    "url": "/reviews-previewer/static/js/runtime-main.68eafc6c.js"
+  }
+]);
